edd4f11994ee1660736169cfd5fe8a0a32e38b3e
baiyong <baiyonguestc@gmail.com>
2024-04-29 11:51:32 +0800

    implement sentencepiece PreTrainedTokenizer

--- biomlm/models/tokenizer/_sentencepiece_tokenizer.py
+++ biomlm/models/tokenizer/_sentencepiece_tokenizer.py
@@ -26,11 +26,11 @@ reference:
 
 """
 
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional, Tuple, Union
+
 import sentencepiece as spm
 
 from transformers.tokenization_utils import AddedToken, PreTrainedTokenizer
-
 from transformers.utils import logging
 
 logger = logging.get_logger(__name__)
@@ -48,10 +48,13 @@ class BioSeqSPMTokenizer(PreTrainedTokenizer):
     def __init__(
         self,
         vocab_file,
-        do_lower_case=False,
         bos_token="<BOS>",
         eos_token="<EOS>",
         unk_token="<UNK>",
+        add_bos_token: bool=False,
+        add_eos_token: bool=True,
+        do_lower_case: bool=False,
+        add_prefix_space: bool = False,
         sp_model_kwargs: Optional[Dict[str, Any]] = None,
         **kwargs,
     ):
@@ -68,39 +71,194 @@ class BioSeqSPMTokenizer(PreTrainedTokenizer):
         #     "alpha":0.16,
         # }
 
+        self.add_bos_token = add_bos_token
+        self.add_eos_token = add_eos_token
+
         self.do_lower_case = do_lower_case
+        self.add_prefix_space = add_prefix_space
+
+
         self.vocab_file = vocab_file
-        
+        # load the pretrained vocab model
         self.sp_model = spm.SentencePieceProcessor(**self.sp_model_kwargs)
-        self.sp_model.Load(vocab_file)
+        self.sp_model.Load(vocab_file) 
+
+        bos_token = AddedToken(bos_token, lstrip=False, rstrip=False) if isinstance(bos_token, str) else bos_token
+        eos_token = AddedToken(eos_token, lstrip=False, rstrip=False) if isinstance(eos_token, str) else eos_token
+        unk_token = AddedToken(unk_token, lstrip=False, rstrip=False) if isinstance(unk_token, str) else unk_token
+
 
         super().__init__(
-            do_lower_case=do_lower_case,
             bos_token=bos_token,
             eos_token=eos_token,
             unk_token=unk_token,
+            do_lower_case=do_lower_case,
+            add_prefix_space=add_prefix_space,
             sp_model_kwargs=self.sp_model_kwargs,
             **kwargs,
         )
-        
 
-        
-
-
-
+    @property
+    def vocab_size(self):
+        return self.sp_model.vocab_size()
     
+    def get_vocab(self):
+        vocab = {self.convert_ids_to_tokens(i): i for i in range(self.vocab_size)}
+        vocab.update(self.added_tokens_encoder)
+        return vocab
+    
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state["sp_model"] = None
+        return state
 
-# def train_jp(vocab_size):
-#   model_prefix = "cc100_jp" + "_vocab_" + str(vocab_size)
-#   spm.SentencePieceTrainer.train(input=tempfile_path
-#       , model_prefix=model_prefix
-#       , vocab_size=vocab_size
-#       , character_coverage = 0.9995
-#       , num_threads=60
-#       , train_extremely_large_corpus=True
-#   )
-# train_jp(8000)
+    def __setstate__(self, d):
+        self.__dict__ = d
 
-# https://github.com/google/sentencepiece/blob/master/python/README.md
-# spm.SentencePieceTrainer.train(
-#       sentence_iterator=response, model_writer=model, vocab_size=1000)
+        # for backward compatibility
+        if not hasattr(self, "sp_model_kwargs"):
+            self.sp_model_kwargs = {}
+
+        self.sp_model = spm.SentencePieceProcessor(**self.sp_model_kwargs)
+        self.sp_model.Load(self.vocab_file)
+    
+    def preprocess_seq(self, inputs):
+        if self.do_lower_case:
+            inputs = inputs.lower()
+        return inputs
+    
+    def _tokenize(self, raw_seq: str):
+        """
+        Converts a string into a sequence of tokens (string), using the tokenizer. Split in words for word-based
+        vocabulary or sub-words for sub-word-based vocabularies (BPE/SentencePieces/WordPieces).
+
+        Do NOT take care of added tokens.
+        """
+        raw_seq = self.preprocess_seq(raw_seq)
+        pieces = self.sp_model.encode(raw_seq, out_type=str)
+
+        return pieces
+    
+    def _convert_token_to_id(self, token):
+        return self.sp_model.PieceToId(token)
+    
+    def _convert_id_to_token(self, index: int) -> str:
+        return self.sp_model.IdToPiece(index)
+
+    # def _decode(self, token_ids: List[int], skip_special_tokens: bool = False, clean_up_tokenization_spaces: bool = None, spaces_between_special_tokens: bool = True, **kwargs) -> str:
+    #     return super()._decode(token_ids, skip_special_tokens, clean_up_tokenization_spaces, spaces_between_special_tokens, **kwargs)
+
+    def build_inputs_with_special_tokens(
+        self, 
+        token_ids_0: List[int], 
+        token_ids_1: Optional[List[int]] = None
+    ) -> List[int]:
+        """
+        Build model inputs from a sequence or a pair of sequence for sequence classification tasks by concatenating and
+        adding special tokens when `add_special_tokens`=True in `__call__` function, which will call `tokenize` function 
+        with the same of `add_special_tokens` parameter. A sequence has the following format:
+
+        - single sequence: `<BOS> X <EOS>`
+        - pair of sequences: `<BOS> A <EOS> B <EOS>`
+
+        Args:
+            token_ids_0 (`List[int]`):
+                List of IDs to which the special tokens will be added.
+            token_ids_1 (`List[int]`, *optional*):
+                Optional second list of IDs for sequence pairs.
+
+        Returns:
+            `List[int]`: List of [input IDs](../glossary#input-ids) with the appropriate special tokens.
+        """
+
+        bos_token_id = [self.bos_token_id] if self.add_bos_token else []
+        eos_token_id = [self.eos_token_id] if self.add_eos_token else []
+
+        output = bos_token_id + token_ids_0 + eos_token_id
+
+        if token_ids_1 is not None:
+            output = output + token_ids_1 + eos_token_id
+
+        return output
+    
+    def get_special_tokens_mask(
+        self, 
+        token_ids_0: List[int], 
+        token_ids_1: Optional[List[int]] = None, 
+        already_has_special_tokens: bool = False
+    ) -> List[int]:
+        """
+        Retrieve sequence ids from a token list that has no special tokens added. This method is called when adding
+        special tokens using the tokenizer `prepare_for_model` method.
+
+        Args:
+            token_ids_0 (`List[int]`):
+                List of IDs.
+            token_ids_1 (`List[int]`, *optional*):
+                Optional second list of IDs for sequence pairs.
+            already_has_special_tokens (`bool`, *optional*, defaults to `False`):
+                Whether or not the token list is already formatted with special tokens for the model.
+
+        Returns:
+            `List[int]`: A list of integers in the range [0, 1]: 1 for a special token, 0 for a sequence token.
+        """
+
+        if already_has_special_tokens:
+            return super().get_special_tokens_mask(
+                token_ids_0=token_ids_0, token_ids_1=token_ids_1, already_has_special_tokens=True
+            )
+        
+        bos_token_id = [1] if self.add_bos_token else []
+        eos_token_id = [1] if self.add_eos_token else []
+
+        if token_ids_1 is None:
+            return bos_token_id + ([0] * len(token_ids_0)) + eos_token_id
+        
+        return (
+            bos_token_id
+            + ([0] * len(token_ids_0))
+            + eos_token_id
+            + ([0] * len(token_ids_1))
+            + eos_token_id
+        )
+    
+    def create_token_type_ids_from_sequences(
+        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
+    ) -> List[int]:
+        """
+        Creates a mask from the two sequences passed to be used in a sequence-pair classification task. An 
+        sequence pair mask has the following format:
+
+        ```
+        0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+        | first sequence      | second sequence |
+        ```
+
+        if token_ids_1 is None, only returns the first portion of the mask (0s).
+
+        Args:
+            token_ids_0 (`List[int]`):
+                List of ids.
+            token_ids_1 (`List[int]`, *optional*):
+                Optional second list of IDs for sequence pairs.
+
+        Returns:
+            `List[int]`: List of [token type IDs](../glossary#token-type-ids) according to the given sequence(s).
+        """
+
+        bos_token_id = [self.bos_token_id] if self.add_bos_token else []
+        eos_token_id = [self.eos_token_id] if self.add_eos_token else []
+        
+        output = len(bos_token_id + token_ids_0 + eos_token_id) * [0]
+
+        if token_ids_1 is not None:
+            output += len(token_ids_1 + eos_token_id) * [1]
+        
+        return output
+
+
+    def prepare_for_tokenization(self, seq, is_split_into_words=False, **kwargs):
+        add_prefix_space = kwargs.pop("add_prefix_space", self.add_prefix_space)
+        if (is_split_into_words or add_prefix_space) and (len(seq) > 0 and not seq[0].isspace()):
+            seq = " " + seq
+        return (seq, kwargs)
