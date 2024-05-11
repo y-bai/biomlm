8518127de6f48b380828febd5153701b7aa67d9e
baiyong <baiyonguestc@gmail.com>
2024-05-11 09:01:19 +0800

    rename script file

--- biomlm/train/run_bioseqmamba_train.sh
+++ biomlm/train/run_bioseqmamba_train.sh
@@ -1,7 +1,7 @@
 # 
 # (C)Copyright 2023-2024: Yong Bai, baiyong at genomics.cn
 
-nohup torchrun --nproc_per_node 4 run_bioseqmamba_causal.py > train.log &
+nohup torchrun --nproc_per_node 4 run_bioseqmamba_train.py > train.log &
 
 # if use one GPU, set os.environ["CUDA_VISIBLE_DEVICES"] = "0" in front of .py script.
 # nohup python run_bioseqmamba_causal.py > train.log &
